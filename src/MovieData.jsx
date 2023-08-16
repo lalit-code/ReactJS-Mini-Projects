d9da8fd24dd1fea186365669b46ff352185e2816
lalit-code <lalitsinghal1234@gmail.com>
2023-08-16 18:05:38 +0530

    update photo

--- src/MovieData.jsx
+++ src/MovieData.jsx
@@ -27,7 +27,7 @@ const MovieDATA = [
     },
         {
         name: "Mirzapur",
-        srclink: "https://talenthouse-res.cloudinary.com/image/upload/c_limit,f_auto,fl_progressive,h_1280,w_1280/v1542762797/user-659794/submissions/poa4rzcivchitmczoyaq.jpg",
+        srclink: "https://flxt.tmsimg.com/assets/p18962858_b_v10_aa.jpg",
         mainlink: "https://www.primevideo.com/detail/0TZXBY68IEI9IGJD20LSQUOEDJ?ref_=dvm_pds_g_in_kk_s_gt_149382470633_mkw_sTQ4NGzX9vn-dc&mrntrk=pcrid_658205142622_slid__pgrid_149382470633_pgeo_9061652_x__adext__ptid_kwd-300369463402&gclid=Cj0KCQjw98ujBhCgARIsAD7QeAiOtp3Li1CCgfiz6OUWvqwytO1STuW4OppZ_eaJ9faqNX1NudVpt6QaAtNbEALw_wcB&gclsrc=aw.ds",
         platform: "Amazon Prime Original"
     },
@@ -49,7 +49,39 @@ const MovieDATA = [
         srclink: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTQjUd1HJYMd9w_d7N3Lt7wTe81lxILhK-beqvKGsAvE6urJOTY",
         mainlink: "https://www.netflix.com/in/title/80115328",
         platform: "A NETFLIX Original"
-    }
+    },
+    {
+    name: "Patal Lok",
+    srclink: "https://m.media-amazon.com/images/M/MV5BMTE5NWUyMmYtMWE1My00ZDhiLWExZjEtMGJjYTA0OGYwZjIwXkEyXkFqcGdeQXVyODQ5NDUwMDk@._V1_.jpg",
+    mainlink: "https://www.primevideo.com/dp/amzn1.dv.gti.ecb8e6b7-a43d-5359-5152-4c31c68b977a?autoplay=0&ref_=atv_cf_strg_wb",
+    platform: "Amazon Prime Original"
+},
+    {
+    name: "Mirzapur",
+    srclink: "https://flxt.tmsimg.com/assets/p18962858_b_v10_aa.jpg",
+    mainlink: "https://www.primevideo.com/detail/0TZXBY68IEI9IGJD20LSQUOEDJ?ref_=dvm_pds_g_in_kk_s_gt_149382470633_mkw_sTQ4NGzX9vn-dc&mrntrk=pcrid_658205142622_slid__pgrid_149382470633_pgeo_9061652_x__adext__ptid_kwd-300369463402&gclid=Cj0KCQjw98ujBhCgARIsAD7QeAiOtp3Li1CCgfiz6OUWvqwytO1STuW4OppZ_eaJ9faqNX1NudVpt6QaAtNbEALw_wcB&gclsrc=aw.ds",
+    platform: "Amazon Prime Original"
+},
+    {
+    name: "Hostel Daze",
+    srclink: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRXTvlfSKHv26m6DMOk0ezLRlHtdGkhHZD1_BQ24SqTyCdtcHbl",
+    mainlink: "https://www.primevideo.com/detail/0JXMZ6NEU5AKLG06AD46X9OVER/?ref_=dvm_pds_gen_in_as_s_gt_7249_mkw_s4PtDH6Mc-dc&mrntrk=pcrid_452352830797_slid__pgrid_109020874727_pgeo_9061652_x__adext__ptid_kwd-929366235254&gclid=Cj0KCQjw98ujBhCgARIsAD7QeAhKum3pgkGpvaUSK0OEZUwiMcJpMcYtBFATzsYLPCFQyJQ5sRP0-SEaAukREALw_wcB",
+    platform: "Amazon Prime Original"
+},
+
+    {
+    name: "Drishyam",
+    srclink: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTkMKFMrv9gfroA4CC9c9C_d8I1NrhiGgVhYGL7xMWO5oylqi4_",
+    mainlink: "https://www.hotstar.com/in/movies/drishyam/1000074189",
+    platform: "Disney+ Hotstar Original"
+},
+    {
+    name: "Sacred Games",
+    srclink: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTQjUd1HJYMd9w_d7N3Lt7wTe81lxILhK-beqvKGsAvE6urJOTY",
+    mainlink: "https://www.netflix.com/in/title/80115328",
+    platform: "A NETFLIX Original"
+
+}
 ]
 
 
